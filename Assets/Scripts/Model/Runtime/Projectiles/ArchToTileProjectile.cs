bbd38273811e7432af486d0320cc873cb1fcaa56
AlexanderSolomatin <alexandrsol23@gmail.com>
2024-05-22 21:19:53 +0300

    hw-2_new_update

--- Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
+++ Assets/Scripts/Model/Runtime/Projectiles/ArchToTileProjectile.cs
@@ -31,7 +31,7 @@ namespace Model.Runtime.Projectiles
             ///////////////////////////////////////
             float maxHeight = totalDistance * 0.6f;
             localHeight = maxHeight * (-(t * 2 - 1) * (t * 2 - 1) + 1);
-
+            // SOME TEXT
 
 
             ///////////////////////////////////////
